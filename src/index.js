b56d140087f5ad879b0d1dfc93ece75e48e05ecb
Jurgis Bridzius <jurgis@bridzius.lt>
2017-01-05 22:39:32 +0200

    Random colours! WOo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import {Color} from "./color"
-var color = new Color(255, 255, 100);
-console.log(color);
+import {Color} from "./color";
+let randomRed = Math.ceil(Math.random() * 255);
+let randomGreen = Math.ceil(Math.random() * 255);
+let randomBlue = Math.ceil(Math.random() * 255);
+let color = new Color(randomRed, randomGreen, randomBlue);
+document.body.style.color = color.hex;
+document.body.style.backgroundColor = color.changeHue(Math.random() < 0.5 ? 100 : -100);
